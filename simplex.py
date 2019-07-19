73de53f83ac97c8c004c679ab12c416e36d8489e
Ling Xiao Lu <lingxiao.lu@mail.utoronto.ca>
2019-07-19 19:47:53 -0400

    second commit

--- simplex.py
+++ simplex.py
@@ -1,4 +1,3 @@
-import numpy
 import sys
 from math import inf
 
@@ -11,9 +10,13 @@ All of A, b and c are implemented with dictionaries instead of arrays.
 v is wrapped in c[-1] so that it can be modified in place.
 In the slack form, A.keys() is equivalent to B, and A[i].keys() is equivalent to N, so both variables are
 omitted. Additionally, b.keys() is equivalent to B, c.keys() is equivalent to N.
+
+Author: lvlingxiao1
 '''
 
 INSTRUCTION = '''
+Usage:
+    python simplex.py filename [-no]
 Use -no flag to disable show-steps.
 The input should be in a separate file with the following format.
 The first line contains two number: number of variables n and number of constraints m
@@ -35,59 +38,54 @@ The input should be
 0 0 1
 5 3 1 4
 1 2 0.5
-Note that index starts from 0 instead of 1 in the algorithm, so they are actually x0, x1, x2
+Note that variable index starts from 0 instead of 1 in the algorithm, so they will actually become x0, x1, x2
 '''
 
 SHOW_STEPS = True
 
 
 def parse_input(s):
+    '''
+    convert input string in standard form to A, b, c in slack form with the relationship:
+        z = c[-1] + c*x
+        B = b + A*N
+    where N is the vector of non-basic variables, B is the vector of basic variables, b is the constant variable,
+    and A is the matrix, that has the opposite signs as in the standard form.
+    A is implemented with a 2D dictionary, b and c are implemented with 1D dictionary.
+    The keys of A and b are equivalent to B, and keys of A[i] and c are equivalent to N
+    :param s: input string
+    :return: A, b, c, num_var
+    '''
     s = s.split('\n')
     row = s[0].split()
     try:
         num_var = int(row[0])
         num_con = int(row[1])
 
-        A = []
+        A = {}
         for i in range(num_con):
+            A[i + num_var] = {}  # basic variable starts from index num_var
             row = s[i + 1].split()
-            A.append([])
-            for j in range(num_var):
-                A[i].append(float(row[j]))
+            for j in range(num_var):  # non-basic variables from 0 to num_var-1
+                A[i + num_var][j] = - float(row[j])  # negative in slack form
 
-        b = []
+        b = {}
         row = s[num_con + 1].split()
         for i in range(num_con):
-            b.append(float(row[i]))
+            b[i + num_var] = float(row[i])
 
-        c = []
+        c = {}
         row = s[num_con + 2].split()
         for i in range(num_var):
-            c.append(float(row[i]))
+            c[i] = float(row[i])
+
+        c[-1] = 0  # constant in the objective function, "v" in the textbook
+
         return A, b, c, num_var
 
     except ValueError:
         print('Input error.' + INSTRUCTION)
         sys.exit(1)
-
-
-def to_slack_form(std_A, std_b, std_c, num_var):
-    A = {}
-    b = {}
-    for index, row in enumerate(std_A):
-        i = num_var + index
-        A[i] = dict()
-        for j, val in enumerate(row):
-            A[i][j] = -val
-        b[i] = std_b[index]
-
-    c = {}
-    for i in range(num_var):
-        c[i] = std_c[i]
-
-    c[-1] = 0  # c[-1]=v in the textbook, need a pointer for modifying in place
-
-    return A, b, c
 
 
 def print_slack(A, b, c):
@@ -111,11 +109,15 @@ def can_improve(c):
     return -1
 
 
-def simplex(A, b, c, num_var):
-    A, b, c = to_slack_form(A, b, c, num_var)
+def simplex(s):
+    '''
+    :param s: input string, format is specified in the instructions
+    :return: None, solution will be printed
+    '''
+    A, b, c, num_var = parse_input(s)
     print_slack(A, b, c)
-    # if any(i < 0 for i in B):
-    #     get_first_feasible_solution(A, b, c)
+    if any(b[i] < 0 for i in b):
+        first_feasible_solution(A, b, c)
     e = can_improve(c)
     while e != -1:
         tightest_bound = inf
@@ -131,66 +133,120 @@ def simplex(A, b, c, num_var):
             sys.exit(0)
         pivot(A, b, c, e, l)
 
-        if SHOW_STEPS:
-            print_slack(A, b, c)
-
         e = can_improve(c)
 
     print("\nSolution:")
     for i in range(num_var):
         if i in b:
-            print(f'x{i}: {b[i]}')
+            print(f'x{i} = {b[i]}')
         else:
-            print(f'x{i}: 0')
+            print(f'x{i} = 0')
+    print(f'z = {c[-1]}')
 
 
 def pivot(A, b, c, e, l):
-    print(f'==>Pivot on x{e} with row of x{l}')
+    if SHOW_STEPS:
+        print(f'==>Pivot on x{e} with row of x{l}')
     b[e] = - b[l] / A[l][e]
     del b[l]
 
     A[e] = {}
     for i in A[l]:
         if i != e:
-            A[e][i] = -A[l][i]/A[l][e]
-    A[e][l] = 1/A[l][e]
+            A[e][i] = -A[l][i] / A[l][e]
+    A[e][l] = 1 / A[l][e]
     del A[l]
 
     for i in A:
         if i != e:
-            b[i] += A[i][e]*b[e]
+            b[i] += A[i][e] * b[e]
             for j in A[i]:
                 if j != e:
-                    A[i][j] += A[i][e]*A[e][j]
+                    A[i][j] += A[i][e] * A[e][j]
             A[i][l] = A[i][e] * A[e][l]
             del A[i][e]
 
-    c[-1] += c[e]*b[e]
+    c[-1] += c[e] * b[e]
     for i in c:
         if i != e and i != -1:
-            c[i] += c[e]*A[e][i]
-    c[l] = c[e]*A[e][l]
+            c[i] += c[e] * A[e][i]
+    c[l] = c[e] * A[e][l]
     del c[e]
+
+    if SHOW_STEPS:
+        print_slack(A, b, c)
+
+
+def first_feasible_solution(A, b, c):
+    new_var = len(A) + len(c) - 1  # index = 1 + max_index
+
+    for i in A:
+        A[i][new_var] = 1
+
+    c[new_var] = 1
+
+    tightest_bound = 0
+    l = None
+    for i in A:
+        if b[i] < tightest_bound:
+            tightest_bound = b[i]
+            l = i
+
+    if SHOW_STEPS:
+        print(f'Add artificial variable x{new_var}')
+        print_slack(A, b, c)
+
+    pivot(A, b, c, new_var, l)
+
+    e = None
+    for i in A[new_var]:
+        if A[new_var][i] < 0:
+            e = i
+            break
+    if e is None:
+        print('This linear program has no feasible solution.')
+        sys.exit(0)
+
+    pivot(A, b, c, e, new_var)
+
+    del c[new_var]
+    for i in A:
+        del A[i][new_var]
+
+    if SHOW_STEPS:
+        print(f'Remove artificial variable x{new_var}')
+        print_slack(A, b, c)
+
+
+EXAMPLE = '''3 4    # 3 variables and 4 constraints
+1 1 1
+1 0 0
+0 1 0
+0 0 1
+5 3 1 4     # this line contains vector b
+1 2 0.5     # this line contains vector c
+'''
+
+EXAMPLE2 = '''2 3
+1 -1
+-1 -1
+-1 4
+8 -3 2
+1 3
+'''
 
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        print('Usage:\n\tpython simplex.py filename [-no]')
-        # print(INSTRUCTION)
-        EXAMPLE = '''3 4    # 3 variables and 4 constraints
-                1 1 1
-                1 0 0
-                0 1 0
-                0 0 1
-                5 3 1 4     # this line contains vector b
-                1 2 0.5     # this line contains vector c
-                '''
-        s = EXAMPLE
+        print(INSTRUCTION)
+        s = EXAMPLE2
+        print("Example result:")
+        simplex(s)
     else:
         f = open(sys.argv[1])
         s = f.read()
         if len(sys.argv) >= 3:
-            if sys.argv[2].startswith('-no'):
+            if sys.argv[2].startswith('-n'):
                 SHOW_STEPS = False
-    A, b, c, num_var= parse_input(s)
-    simplex(A, b, c, num_var)
+        simplex(s)
+
